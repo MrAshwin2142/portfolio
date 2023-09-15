10c206c55bebe0c83c39f75f4eb1985944d50482
MrAshwin2142 <ashwindhangar2142002@gmail.com>
2023-09-15 12:18:15 +0530

    adding eduction section

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function alertFunction(){
-    alert("working on it will be added soon..");
+const main=document.getElementById("main-container");
+const education=document.getElementById("education");
+const skils=document.getElementById("skills");
+const projects=document.getElementById("projects");
+const more = document.getElementById("more"); 
+function alertFunction(section){
+    main.style.display="none";
+    education.style.display="none";
+    skils.style.display="none";
+    projects.style.display="none";
+    more.style.display="none";
+    section.style.display="block";
 }
