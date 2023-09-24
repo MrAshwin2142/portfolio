efbab715f6435024ff5a09c1823ff211e7324d01
MrAshwin2142 <ashwindhangar2142002@gmail.com>
2023-09-24 20:40:32 +0530

    adding skill and project section

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const main=document.getElementById("main-container");
 const education=document.getElementById("education");
 const skils=document.getElementById("skills");
 const projects=document.getElementById("projects");
-const more = document.getElementById("more"); 
+// const more = document.getElementById("more");
+const theme = document.querySelector(':root');
+
 function alertFunction(section){
+    // console.log("theme");
     main.style.display="none";
     education.style.display="none";
     skils.style.display="none";
     projects.style.display="none";
-    more.style.display="none";
+    // more.style.display="none";
     section.style.display="block";
 }
